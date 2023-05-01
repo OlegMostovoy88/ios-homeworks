fc4cf7077ed9f24d6afe525f5ded1721fca4f54d
OlegMostovoy88 <118436356+OlegMostovoy88@users.noreply.github.com>
2023-05-01 13:39:33 +0300

    diploma finish

--- Navigation/Controllers/TabBarController.swift
+++ Navigation/Controllers/TabBarController.swift
@@ -6,8 +6,6 @@ final class TabBarController: UITabBarController {
     private let profileNavigationController = UINavigationController(rootViewController: ProfileViewController())
     private let postViewController = UINavigationController(rootViewController: PostViewController())
     private let logInViewController = UINavigationController(rootViewController: LogInViewController())
-    private let photosViewController = UINavigationController(rootViewController: PhotosViewController())
-    private let notificationsViewController = UINavigationController(rootViewController: NotificationsViewController())
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +19,10 @@ final class TabBarController: UITabBarController {
         UITabBar.appearance().tintColor = #colorLiteral(red: 0.210213542, green: 0.0632795915, blue: 0.5209727883, alpha: 1)
         
         
-        viewControllers = [logInViewController, photosViewController, notificationsViewController, feedNavigationController]
+        viewControllers = [logInViewController, feedNavigationController]
         feedNavigationController.tabBarItem.title = "Лента"
         feedNavigationController.tabBarItem.image = UIImage(systemName: "doc.circle")
         logInViewController.tabBarItem.title = "Профиль"
         logInViewController.tabBarItem.image = UIImage(systemName: "person.circle")
-        photosViewController.tabBarItem.title = "Фото"
-        photosViewController.tabBarItem.image = UIImage(systemName: "photo.circle")
-        notificationsViewController.tabBarItem.title = "Уведомления"
-        notificationsViewController.tabBarItem.image = UIImage(named: "notifications")
     }
 }
